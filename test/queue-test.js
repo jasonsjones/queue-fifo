03323e8684b1ce112322f4978e5f48ce22f67d1f
Jason <jasonjones@salesforce.com>
2017-12-02 15:56:12 -0800

    Refactor tests to use chai expect assertions

--- test/queue-test.js
+++ test/queue-test.js
@@ -2,10 +2,9 @@
 var chai = require('chai');
 var Queue = require('../');
 
-chai.should();
+var expect = chai.expect;
 
-describe('Queue Unit Tests', function() {
-
+describe('Queue data structure', function() {
     var queue;
 
     beforeEach(function () {
@@ -16,60 +15,56 @@ describe('Queue Unit Tests', function() {
         queue = null;
     });
 
-    it('should have a working test environment', function() {
-        true.should.equal(true);
+    it('instantiates a queue instance', function () {
+        expect(queue).to.exist;
     });
 
-    it('should instantiate a queue instance', function () {
-        queue.should.be.ok;
+    it('is empty when first instantiated', function () {
+        expect(queue.isEmpty()).to.be.true;
+        expect(queue.size()).to.equal(0);
     });
 
-    it('should be empty when first instantiated', function () {
-        queue.isEmpty().should.equal(true);
-        queue.size().should.equal(0);
-    });
-
-    it('should queue up data at the back of queue', function () {
+    it('queues up data at the back of queue', function () {
         queue.enqueue('some test data');
         queue.enqueue('some more test data');
         queue.enqueue('and yet some more...');
-        queue.size().should.equal(3);
+        expect(queue.size()).to.equal(3);
     });
 
-    it('should dequeue data from the front of the queue', function () {
+    it('dequeues data from the front of the queue', function () {
         queue.enqueue('some test data');
         queue.enqueue('some more test data');
         queue.enqueue('and yet some more...');
-        queue.size().should.equal(3);
+        expect(queue.size()).to.equal(3);
 
         var first = queue.dequeue();
-        first.should.equal('some test data');
-
-        queue.size().should.equal(2);
-
-        queue.dequeue().should.equal('some more test data');
-        queue.size().should.equal(1);
+        expect(first).to.equal('some test data');
+        expect(queue.size()).to.equal(2);
+        expect(queue.dequeue()).to.equal('some more test data');
+        expect(queue.size()).to.equal(1);
     });
 
-    it('should peek at the data at the front of the queue', function () {
+    it('peeks at the data at the front of the queue', function () {
         queue.enqueue('some test data');
         queue.enqueue('some more test data');
         queue.enqueue('and yet some more');
         queue.enqueue('and even more data');
-        queue.size().should.equal(4);
+
+        expect(queue.size()).to.equal(4);
         var first = queue.peek();
-        first.should.equal('some test data');
-        queue.size().should.equal(4);
+        expect(first).to.equal('some test data');
+        expect(queue.size()).to.equal(4);
     });
 
-    it('should clear the queue of all data', function () {
+    it('clears the queue of all data', function () {
         queue.enqueue('some test data');
         queue.enqueue('some more test data');
         queue.enqueue('and yet some more');
         queue.enqueue('and even more data');
-        queue.size().should.equal(4);
+
+        expect(queue.size()).to.equal(4);
         queue.clear();
-        queue.size().should.equal(0);
-        queue.isEmpty().should.equal(true);
+        expect(queue.size()).to.equal(0);
+        expect(queue.isEmpty()).to.be.true;
     });
 });
